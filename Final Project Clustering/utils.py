923a91746bdbeca5f77bd5968cf0069412e39c5c
marridG <marridG@sjtu.edu.cn>
2023-03-29 23:29:38 +0800

    add utils [SUBJECT] dataloader, color getter - [TESTED] YES - [ADDED] utils.py - [EDITED] - [RMVED]

---
+++ Final Project Clustering/utils.py
@@ -0,0 +1,88 @@
+"""
+reference:
+    1. https://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_comparison.html
+"""
+
+from itertools import cycle, islice
+import numpy as np
+from sklearn import datasets
+
+np.random.seed(0)
+
+# ============
+# Generate datasets. We choose the size big enough to see the scalability
+# of the algorithms, but not too big to avoid too long running times
+# ============
+n_samples = 500
+noisy_circles = datasets.make_circles(n_samples=n_samples, factor=0.5, noise=0.05)
+noisy_moons = datasets.make_moons(n_samples=n_samples, noise=0.05)
+blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
+no_structure = np.random.rand(n_samples, 2), None
+
+# Anisotropicly distributed data
+random_state = 170
+X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+transformation = [[0.6, -0.6], [-0.4, 0.8]]
+X_aniso = np.dot(X, transformation)
+aniso = (X_aniso, y)
+
+# blobs with varied variances
+varied = datasets.make_blobs(
+    n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=random_state
+)
+
+datasets = [
+    noisy_circles,
+    noisy_moons,
+    varied,
+    aniso,
+    blobs,
+    no_structure,
+]
+
+# ============
+# Generate colors for clusters
+# ============
+colors = cycle(
+    [
+        "#377eb8",
+        "#ff7f00",
+        "#4daf4a",
+        "#f781bf",
+        "#a65628",
+        "#984ea3",
+        "#999999",
+        "#e41a1c",
+        "#dede00",
+    ]
+)
+
+
+def get_dataset_list():
+    return datasets
+
+
+def get_colors_by_n_cluster(n_cluster: int) -> np.ndarray:
+    res = np.array(
+        list(
+            islice(
+                cycle(
+                    [
+                        "#377eb8",
+                        "#ff7f00",
+                        "#4daf4a",
+                        "#f781bf",
+                        "#a65628",
+                        "#984ea3",
+                        "#999999",
+                        "#e41a1c",
+                        "#dede00",
+                    ]
+                ),
+                n_cluster,
+            )
+        )
+    )
+    # add black color for outliers (if any)
+    res = np.append(res, ["#000000"])
+    return res
